70652f13058515ed17a1b557912027375d6bee52
Luciano Silva <lcnsilvajf@gmail.com>
2025-06-12 21:51:37 -0300

    feat: implement Home page layout and Button component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,34 @@
+import { 
+    ButtonContainer,
+    Description,
+    HomeContainer,
+    Logo,
+    Subtitle,
+    TextContainer
+} from "./style";
+
+import LogoIcon from '../../assets/logo.jpg'
+import Button from "../../components/UI/Button";
+
 const Home = () => {
+
     return (
-        <>
-        test
-        </>
+        <HomeContainer>
+            <Logo src={LogoIcon} />
+            <TextContainer>
+                <Subtitle>Bem-vindo ao seu cuidado completo para os pés</Subtitle>  
+                <Description>
+                    Descubra uma nova forma de cuidar da saúde dos seus pés com a Smartt Pés. 
+                    Agende consultas, acompanhe seu tratamento e receba dicas personalizadas para manter seus pés sempre saudáveis.
+                </Description>
+            </TextContainer>
+            
+            <ButtonContainer>
+                <Button text="Criar conta" bgcolor="#DBE8F2" color="#2E3337" endpoint="/criar"/>
+                <Button text="Entrar" endpoint="/login"/>
+            </ButtonContainer>
+
+        </HomeContainer>
     )
 }
 
